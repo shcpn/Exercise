9232198e40e779714af37130add1086866ca3f3c
hosseini <hamid.hosseini@ENIAC-TECH.LOCAL>
2021-09-05 03:55:06 +0430

    V1.01

--- UpTime_robot.py
+++ UpTime_robot.py
@@ -1,1 +1,12 @@
 #this is UpTime_robot.py for automation function
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time 
+
+mydriver=webdriver.Firefox()
+mydriver.get("https://uptimerobot.com/")
+Application_Manager_NOC=webdriver.Firefox()
+Application_Manager_NOC.get("192.168.15.247:9090")
+#print(mydriver.title)
+#print(mydriver.current_url)
+mydriver.close()
