e213ee4258ca732ee0cb52f4d848176734f4a9ba
hamid <sh.cpn@engineer.com>
2021-10-03 03:35:10 +0330

    1- fix the line problem 2- fix the owerwrite problem

--- input_url.py
+++ input_url.py
@@ -1,41 +1,50 @@
 import csv
 import pandas as pd
+import os.path
 
 header = ['firefox_webdriver', 'link', 'username_element_ID', 'username', 'password_element_ID', 'password', 'login_button_ID']
 monitoring = []
 start = input("do you want to start monitor(Yes/No): ")
 csv_PATH = str(input("please enter your csv file path: "))
-while start == "yes":
-    new = input("do you want to add new URL(Yes/No): ")
-    while new == "yes":
-        print ("please enter your link: ")
-        link = str(input())
-        print("please enter the username_element_ID: ")
-        username_element_ID = str(input())
-        print("please enter your username: ")
-        username = str(input())
-        print("please enter the password_element_ID: ")
-        password_element_ID = str(input())
-        print("please enter your password: ")
-        password = str(input())
-        print("please enter the login_button_ID: ")
-        login_button_ID = str(input())
-        monitoring = ["firefox_webdriver", link, username_element_ID, username, password_element_ID, password, login_button_ID]
-        print (monitoring)
-        with open(csv_PATH, "a", encoding='UTF8', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(header)
-            writer.writerow(monitoring)
-            f.close()
-        cont = input("do you want to continue or enugh(continue/enugh): ")
-        if cont == "cont":
-            continue
-        else:
+if os.path.isfile(csv_PATH):
+    df = pd.read_csv(csv_PATH)
+    print (df)
+else:
+    print("the csv_PATH is not create")
+    with open(csv_PATH, "a", encoding='UTF8', newline='') as new:
+        writer = csv.writer(new)
+        writer.writerow(header)
+        new.close()
+    while start == "yes":
+        new = input("do you want to add new URL(Yes/No): ")
+        while new == "yes":
+            print ("please enter your link: ")
+            link = str(input())
+            print("please enter the username_element_ID: ")
+            username_element_ID = str(input())
+            print("please enter your username: ")
+            username = str(input())
+            print("please enter the password_element_ID: ")
+            password_element_ID = str(input())
+            print("please enter your password: ")
+            password = str(input())
+            print("please enter the login_button_ID: ")
+            login_button_ID = str(input())
+            monitoring = ["firefox_webdriver", link, username_element_ID, username, password_element_ID, password, login_button_ID]
+            print (monitoring)
+            with open(csv_PATH, "a", encoding='UTF8', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(monitoring)
+                f.close()
+                cont = input("do you want to continue or enugh(continue/enugh): ")
+                if cont == "cont":
+                    continue
+                else:
+                    break
+        if new == "no":
+            df = pd.read_csv(csv_PATH)
+            print (df)
             break
-    if new == "no":
-        df = pd.read_csv(csv_PATH)
-        print(df)
-        break
 
 
 
