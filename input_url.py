50ae640a6a1c6153cb03c3ca06f70fa29088f5fd
hamid <sh.cpn@engineer.com>
2021-10-06 21:47:28 +0330

    rsa edite

--- input_url.py
+++ input_url.py
@@ -22,29 +22,29 @@ else:
         while new == "yes":
             print ("please enter your link: ")
             link = str(input())
-            R_link = rsa.encrypt(link.encode(), publicKey)
-            print(R_link)
+#            R_link = rsa.encrypt(link.encode(), publicKey)
+            print(link)
             print("please enter the username_element_ID: ")
             username_element_ID = str(input())
-            R_username_element_ID = rsa.encrypt(username_element_ID.encode(), publicKey)
-            print(R_username_element_ID)
+#            R_username_element_ID = rsa.encrypt(username_element_ID.encode(), publicKey)
+            print(username_element_ID)
             print("please enter your username: ")
             username = str(input())
-            R_username = rsa.encrypt(username.encode(), publicKey)
-            print (R_username)
+#            R_username = rsa.encrypt(username.encode(), publicKey)
+            print (username)
             print("please enter the password_element_ID: ")
             password_element_ID = str(input())
-            R_password_element_ID = rsa.encrypt(password_element_ID.encode(), publicKey)
-            print (R_password_element_ID)
+#            R_password_element_ID = rsa.encrypt(password_element_ID.encode(), publicKey)
+            print (password_element_ID)
             print("please enter your password: ")
             password = str(input())
-            R_password = rsa.encrypt(password.encode(), publicKey)
-            print (R_password)
+#            R_password = rsa.encrypt(password.encode(), publicKey)
+            print (password)
             print("please enter the login_button_ID: ")
             login_button_ID = str(input())
-            R_login_button_ID = rsa.encrypt(login_button_ID.encode(), publicKey)
-            print (R_login_button_ID)
-            monitoring = ["firefox_webdriver", R_link, R_username_element_ID, R_username, R_password_element_ID, R_password, R_login_button_ID]
+#            R_login_button_ID = rsa.encrypt(login_button_ID.encode(), publicKey)
+            print (login_button_ID)
+            monitoring = ["firefox_webdriver", link, username_element_ID, username, password_element_ID, password, login_button_ID]
             print (monitoring)
             with open(csv_PATH, "a", encoding='UTF8', newline='') as f:
                 writer = csv.writer(f)
@@ -59,15 +59,3 @@ else:
             df = pd.read_csv(csv_PATH)
             print (df)
             break
-
-
-
-
-
-
-
-#    if input() == "n":
-#        URL = str(input())
-#        link.append(URL)
-#    else:
-#        print ("your URLs are: ", link )
